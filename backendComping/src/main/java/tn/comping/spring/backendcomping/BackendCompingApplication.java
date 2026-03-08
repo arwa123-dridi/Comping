f16b40f841f570f218d028b1c42456d01f56462a
arwadridi <arwadridi624@gmail.com>
2026-03-08 11:30:55 +0100

    Initial commit

---
+++ backendComping/src/main/java/tn/comping/spring/backendcomping/BackendCompingApplication.java
@@ -0,0 +1,13 @@
+package tn.comping.spring.backendcomping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCompingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCompingApplication.class, args);
+    }
+
+}
