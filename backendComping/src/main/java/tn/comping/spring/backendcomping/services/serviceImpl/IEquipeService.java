18ea4b34d31bd816a3840794972c0fd1076833a0
Amal-Melki <MELKI.Amal@esprit.tn>
2026-03-13 03:00:10 +0100

    refactor: Déplacement EquipeServiceImpl et mise à jour imports

--- backendComping/src/main/java/tn/comping/spring/backendcomping/services/serviceImpl/IEquipeService.java
+++ backendComping/src/main/java/tn/comping/spring/backendcomping/services/serviceImpl/IEquipeService.java
@@ -1,4 +1,4 @@
-package tn.comping.spring.backendcomping.services.interfaces;
+package tn.comping.spring.backendcomping.services.serviceImpl;
 
 import tn.comping.spring.backendcomping.dto.EquipeRequestDTO;
 import tn.comping.spring.backendcomping.dto.EquipeResponseDTO;
