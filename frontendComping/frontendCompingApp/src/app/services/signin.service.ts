cdade44e4bb71d98a2d7aead15811135e8107e92
arwadridi <arwadridi624@gmail.com>
2026-03-24 22:14:17 +0100

    Ajout interface login et consommation api login

---
+++ frontendComping/frontendCompingApp/src/app/services/signin.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface LoginDTORequest {
+  username: string;
+  password: string;
+}
+
+export interface LoginDTOResponse {
+  token: string;
+  username: string;
+  roles: string[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SigninService {
+
+ private apiUrl = 'http://localhost:8087/api/auth/login'; 
+
+  constructor(private http: HttpClient) {}
+
+login(dto: { email: string, password: string }): Observable<LoginDTOResponse> {
+  return this.http.post<LoginDTOResponse>(this.apiUrl, dto, {
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
+  saveToken(token: string) {
+    localStorage.setItem('authToken', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+}
