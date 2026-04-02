877d4b928066f0c8b04ac12372efcefd23462983
arwadridi <arwadridi624@gmail.com>
2026-04-02 22:29:08 +0200

    Ajout Dashbord Admin

--- frontendComping/frontendCompingApp/src/app/app.routes.ts
+++ frontendComping/frontendCompingApp/src/app/app.routes.ts
@@ -4,11 +4,20 @@ import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './Home/home/home.component';
 import { ProfileComponent } from './profile/profile.component'; 
 import { SigninComponent } from './signin/signin.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'Campino', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'profile', component: ProfileComponent }, 
   { path: '', redirectTo: 'Campino', pathMatch: 'full' },
-  { path: 'login', component: SigninComponent }
+  { path: 'login', component: SigninComponent },
+   {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent }
+    ]
+  },
 ];
---
+++ frontendComping/frontendCompingApp/src/app/layouts/admin-header/admin-header.component.ts
@@ -0,0 +1,49 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-header',
+  imports: [],
+  templateUrl: './admin-header.component.html',
+  styleUrl: './admin-header.component.css'
+})
+export class AdminHeaderComponent {
+   @Output() sidebarToggle = new EventEmitter<void>();
+ 
+    
+  isDark = false;
+  showNotif = false;
+  showUserMenu = false;
+  notifCount = 2;
+  toggleSidebar(): void {
+    this.sidebarToggle.emit();
+  }
+  toggleTheme(): void {
+    this.isDark = !this.isDark;
+    document.body.classList.toggle('dark', this.isDark);
+  }
+ 
+  toggleNotif(): void {
+    this.showNotif = !this.showNotif;
+    this.showUserMenu = false;
+  }
+ 
+  toggleUserMenu(): void {
+    this.showUserMenu = !this.showUserMenu;
+    this.showNotif = false;
+  }
+ 
+  clearNotifs(): void {
+    this.notifCount = 0;
+    this.showNotif = false;
+  }
+ 
+  closeAll(): void {
+    this.showNotif = false;
+    this.showUserMenu = false;
+  }
+ 
+  logout(): void {
+    // Ajouter la logique de déconnexion ici
+    console.log('Déconnexion...');
+  }
+}
