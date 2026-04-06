76002b113d072f4a1b2e68d1ae6843fea77c7946
arwadridi <arwadridi624@gmail.com>
2026-04-06 16:05:53 +0200

    Ajout interface liste user

---
+++ frontendComping/frontendCompingApp/src/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+   private apiUrl = 'http://localhost:8087/api/users';
+
+   constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<User[]> {
+    const token = localStorage.getItem('token');
+
+  return this.http.get<User[]>(this.apiUrl, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  }
+ deleteUser(id: string): Observable<any> {
+   const token = localStorage.getItem('token');
+
+  return this.http.delete(`${this.apiUrl}/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    responseType: 'text' as 'json'
+  });
+ }}
