77f700f9e956e97156aef4f6b357b7efa963bd63
MimiSell94 <sellamimaryemm@gmail.com>
2026-04-07 04:46:33 +0200

    reset password workflow completion front et back

--- frontendComping/frontendCompingApp/src/app/app.routes.ts
+++ frontendComping/frontendCompingApp/src/app/app.routes.ts
@@ -1,16 +1,18 @@
-
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './Home/home/home.component';
 import { ProfileComponent } from './profile/profile.component'; 
 import { SigninComponent } from './signin/signin.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 export const routes: Routes = [
   { path: 'Campino', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'profile', component: ProfileComponent }, 
   { path: '', redirectTo: 'Campino', pathMatch: 'full' },
-{ path: 'login', component: SigninComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent }
+  { path: 'login', component: SigninComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent }
 ];
+
