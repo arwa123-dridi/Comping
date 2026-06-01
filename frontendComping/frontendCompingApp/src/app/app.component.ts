fabc04f528086d66818b12f211ebdd96ccb172bf
Amal-Melki <MELKI.Amal@esprit.tn>
2026-06-01 16:07:26 +0100

    Mise à jour frontend

--- frontendComping/frontendCompingApp/src/app/app.component.ts
+++ frontendComping/frontendCompingApp/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { EditeEventComponent } from "./client/edite-event/edite-event.component";
-
-
 import { HttpClientModule } from '@angular/common/http';
+
+import { EditeEventComponent } from './client/edite-event/edite-event.component';
+import { Header } from './layouts/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet ,HttpClientModule,EditeEventComponent],
-
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    EditeEventComponent,
+    Header
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontendComping/frontendCompingApp/src/app/services/signup.spec.ts
+++ frontendComping/frontendCompingApp/src/app/services/signup.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { AuthUserService } from './signup.service';
 
-
-describe('Signup', () => {
+describe('AuthUserService', () => {
+  let service: AuthUserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    service = TestBed.inject(AuthUserService);
   });
 
   it('should be created', () => {
